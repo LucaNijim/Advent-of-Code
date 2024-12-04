3e46fef5c030566db1869743689a4e72e30c0e8a
Luca <lnijim@nd.edu>
2024-12-04 12:24:13 +0000

    Reformatted day 3 again

--- 2024/day3.jl
+++ 2024/day3.jl
@@ -5,9 +5,15 @@ pt1(st::String) =
         parse(Int, mul[:n1])*parse(Int,mul[:n2]) 
         for mul in eachmatch(r"mul\((?<n1>[0-9]+),(?<n2>[0-9]+)\)", st)
     )
-pt2(st::String) = 
-    occursin(r"do\(\)", st) ? 
-        pt1(st[match(r"do\(\)", st).offset:match(r"do\(\)", st).offset+match(r"don't\(\)", st[match(r"do\(\)", st).offset:end]).offset+5])+ pt2(st[match(r"do\(\)", st).offset+match(r"don't\(\)", st[match(r"do\(\)", st).offset:end]).offset+5:end]) : 0
+
+function pt2(st::String)
+    if !occursin(r"do\(\)", st)
+        return 0
+    end 
+    doloc = match(r"do\(\)", st).offset
+    dontloc = match(r"don't\(\)", st[doloc:end]).offset + doloc + 5
+    return pt1(st[doloc:dontloc]) + pt2(st[dontloc:end])
+end
 
 print("Part 1 Solution: ")
 println(pt1(s))
